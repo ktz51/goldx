9b551597a2ea6a7baa3c5480a950a6e85c4c1c6b
root <root@box-codeanywhere.com>
2017-07-23 14:25:44 -0400

    initial

--- GoldxEngineer/src/GoldxEngineer/GoldxEngineer.java
+++ GoldxEngineer/src/GoldxEngineer/GoldxEngineer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//wrote by alexsey rebouças
+//written by alexsey rebouças
 //email:alexseyreboucas@icloud.com
 
 package GoldxEngineer;
