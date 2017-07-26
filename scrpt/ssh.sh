b6db961388978b80e3781e941c2573b47141ddb5
root <root@box-codeanywhere.com>
2017-07-26 19:22:21 -0400

    script

---
+++ scrpt/ssh.sh
@@ -0,0 +1,7 @@
+ssh-keygen
+eval `ssh-agent -s`
+ssh-add
+
+#git push out from repository cloning,transform repository in unread mode
+#go back normal on repository cloning and push up..git add -A not git add .
+#commit -a -m ""
